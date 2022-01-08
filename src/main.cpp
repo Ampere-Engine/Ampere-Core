af8afacad93ee1a23d3cd46280342e04c3498d86
Mateusz <mateuszslotwinski16@gmail.com>
2022-01-08 23:15:11 +0100

    FINALLY WORKS

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
+#include <iostream>
 #include <GLFW/glfw3.h>
 
+#define GLFW_DLL
+
 int main() {
+    if (!glfwInit()) {
+        std::cout<<"GLFW initialization failed! \n";
+    } else {
+        std::cout<<"GLFW initialization succeeded! OH MY FUCKING GOD \n";
+    }
     return 0;
 }
