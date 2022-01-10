93bb6365efb180d813a301cd361eaadab7447ffe
Mateusz <mateuszslotwinski16@gmail.com>
2022-01-10 17:23:52 +0100

    I dont know what i am doing, i'm just learning

--- src/graphics/window.cpp
+++ src/graphics/window.cpp
@@ -39,12 +39,12 @@ namespace ampere {
             glfwMakeContextCurrent(m_Window);
             glfwSetWindowSizeCallback(m_Window, windowResize);
 
-            std::cout<<"OpenGL "<<glGetString(GL_VERSION)<<std::endl;
-            
             if(glewInit() != GLEW_OK) {
                 std::cout << "GLEW initialization failed" << std::endl;
                 return false;
             }
+            
+            std::cout<<"OpenGL "<<glGetString(GL_VERSION)<<std::endl;
             
             return true;
         }
--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,21 @@ int main() {
     Window window("Ampeere!", 1400, 700);
     glClearColor(0.0f, 0.6f, 0.8f, 1.0f);
 
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
     while (!window.closed()) {
         window.clear();
+#if 1
+        glBegin(GL_QUADS);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(-0.5f, 0.5f);
+        glVertex2f(0.5f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+#endif
+        glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
         window.update();
     }
 
